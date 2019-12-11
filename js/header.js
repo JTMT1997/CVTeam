8fd790b840e8f3ff7b781ba3ddea00376c4fc704
MalikZone <ridwanmalik2010@gmail.com>
2019-12-11 10:15:08 +0700

    memodif header.html

--- js/header.js
+++ js/header.js
@@ -1,8 +1,8 @@
 const efect = document.getElementById("tron");
 window.addEventListener("scroll", function()
 {
-    let offset = window.pageYOffset;
-    // console.log("offset " + offset);
-    // console.log("offset efect " + offset * 0.7);
-    efect.style.backgroundPositionY = offset * 0.6 + "px";
+    let down = window.pageYOffset;
+    // console.log("down " + offset);
+    // console.log("down efect " + offset * 0.6);
+    efect.style.backgroundPositionY = down * 0.6 + "px";
 });
